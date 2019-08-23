22a55c63d21287760f087cbf5c59f950f273799f
Sojome <sojome@outlook.com>
2019-08-22 22:37:12 -0500

    Completado pero con error de ejecucion

--- EJBAppWeb/Dao/com/dao/MiNacionalidadDao.java
+++ EJBAppWeb/Dao/com/dao/MiNacionalidadDao.java
@@ -8,10 +8,10 @@ import com.ejb.model.MiNacionalidad;
 
 @Local
 public interface MiNacionalidadDao {
-	public MiNacionalidad buscar(MiNacionalidad canton);
-	public String grabar(MiNacionalidad canton);
-	public String actualizar(MiNacionalidad c);
-	public String eliminar(MiNacionalidad c);
+	public MiNacionalidad buscar(MiNacionalidad nacionalidad);
+	public String grabar(MiNacionalidad nacionalidad);
+	public String actualizar(MiNacionalidad n);
+	public String eliminar(MiNacionalidad n);
 	public List<MiNacionalidad> listar(int id);
 	public MiNacionalidad buscarporId(int id);
 }
